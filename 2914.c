df12444c447448feaedb3436c18534d92520f8d8
kissshot1104 <107632424+kissshot1104@users.noreply.github.com>
2022-06-19 18:51:32 +0900

    Update 2914.c

--- 2914.c
+++ 2914.c
@@ -5,16 +5,14 @@ int main()
 {
 	char	str[101]={0};
 	char	dic[][4] = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-	int	len;
-	int	cnt = 0;
+	int		cnt = 0;
 
 	scanf("%s", str);
 	for (int i = 0; i < 8; i++)
 	{
-		len = strlen(dic[i]);
 		for (char *ptr = strstr(str, dic[i]); ptr != 0; ptr = strstr(ptr + 1, dic[i]))
 		{
-			for (int j = 0; j < len; j++)
+			for (int j = 0; j < strlen(dic[i]); j++)
 				ptr[j] = '`';
 			cnt++;
 		}
